1ab543040eab9fb9272ea4a475dc776ddb5d6826
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-14 14:33:04 -0300

    Exercicio - Semana 7 - V.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Header } from "./components/Header";
+import GrupoInput  from "./components/GrupoInput";
+
 
 export const App = () => {
   return (
     <div>
       <Header titulo="Negociações" />
       <div className="container">
+      <GrupoInput />
         {/* OS COMPONENTES SERÃO ENCAIXADOS AQUI! */}
       </div>
     </div>
---
+++ src/components/GrupoInput.jsx
@@ -0,0 +1,22 @@
+function GrupoInput() {
+  
+  return (
+   
+            <div className="row align-item-center col-md-12">
+                <div className="form-group col-md-4">
+                    <label htmlFor="inputData">Data</label>
+                    <input type="date" className="form-control" id="inputData" />
+                </div>
+                <div className="form-group col-md-4">
+                    <label htmlFor="inputQuantidade">Quantidade</label>
+                    <input type="number" className="form-control" id="inputQuantidade" />
+                </div>
+                <div className="form-group col-md-4">
+                    <label htmlFor="inputValor">Valor</label>
+                    <input type="number" className="form-control" id="inputValor" />
+                </div>
+            </div>
+  );
+}
+
+export default GrupoInput;
