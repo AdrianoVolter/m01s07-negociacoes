68b528a4eda30a5a03471397ba65660ef002def1
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-15 21:57:13 -0300

    Proptype no formulario.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,13 @@
 import GrupoInput from "./GrupoInput";
 
-function Formulario(){
+function Formulario(props){
     return(
 
         <form className="form container">
             <div className="row">
                 <GrupoInput/>
                 <div className="form-group col-md-4">
-                    <button className="btn btn-primary mt-4" type="button">Incluir</button>
+                    <button className="btn btn-primary mt-4" type="button">{ props.quandoAdicionar}</button>
                 </div>
             </div>
         </form>
@@ -16,5 +16,10 @@ function Formulario(){
 
 
 }
+
+Formulario.propTypes = {
+    quandoAdicionar: PropTypes.func.isRequired,
+}
+
 
 export default Formulario;
