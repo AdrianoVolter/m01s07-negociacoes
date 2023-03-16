a7655a579e4d9d9d5cd8cafb4c7928dd3f92193d
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-15 21:28:51 -0300

    Tabela exercicio semana 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 // import GrupoInput  from "./components/GrupoInput";
 import Formulario from "./components/Formulario";
+import Tabela from "./components/Tabela";
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <Header titulo="Negociações" />
       <div className="container">
       <Formulario />
+      <Tabela />
         {/* OS COMPONENTES SERÃO ENCAIXADOS AQUI! */}
       </div>
     </div>
---
+++ src/components/Tabela.jsx
@@ -0,0 +1,50 @@
+function Tabela(){
+    return (
+        <table className="table table-hover table-bordered m-2">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>10/10/2018</td>
+                    <td>100</td>
+                    <td>100</td>
+                    <td>10000</td>
+                </tr>
+                <tr>
+                    <td>10/10/2018</td>
+                    <td>100</td>
+                    <td>100</td>
+                    <td>10000</td>
+                </tr>
+                <tr>
+                    <td>10/10/2018</td>
+                    <td>100</td>
+
+                    <td>100</td>
+                    <td>10000</td>
+                </tr>
+                <tr>
+                    <td>10/10/2018</td>
+                    <td>100</td>
+
+                    <td>100</td>
+
+                    <td>10000</td>
+                </tr>
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+        </table>
+
+       
+    )
+}
+
+export default Tabela;
