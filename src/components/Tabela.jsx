2b0f53382d11a47be1f8f0ea6bd5eb7735d1fe57
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-17 12:46:29 -0300

    outros testes

--- src/components/Tabela.jsx
+++ src/components/Tabela.jsx
@@ -1,63 +1,45 @@
+//Configure o componente Tabela com as seguintes props: negociacoes , array, requerido, imprimir cada linha da tabela atraves de um .map detro de tbody;
 import PropTypes from 'prop-types';
 
-function Tabela(props){
+function Tabela(props) {
 
-const negociacoes = ["data", "quantidade", "valor", "volume"]
-    
-    return (
-
-        <table className="table table-hover table-bordered">
-            <thead className='bg bg-primary text-white'>
-                <tr>
-                    <th>Data</th>
-                    <th>Quantidade</th>
-                    <th>Valor</th>
-                    <th>Volume</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.negociacoes.map(negociacao => {
-                        return (
-                            <tr key={negociacao.id}>
-                                <td>{negociacao.data.getDate()}/{negociacao.data.getMonth() + 1}/{negociacao.data.getFullYear()}</td>
-                                <td>{negociacao.quantidade}</td>
-                                <td>{negociacao.valor}</td>
-                                <td>{negociacao.volume}</td>
-                            </tr>
+        return (
+            <table className="table table-hover table-bordered">
+                <thead className='bg bg-primary text-white'>
+                    <tr>
+                        <th>Data</th>
+                        <th>Quantidade</th>
+                        <th>Valor</th>
+                        <th>Volume</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.negociacoes.map(negociacao => {
+                            return (
+                                <tr key={negociacao.id}>
+                                    <td>{negociacao.data.getDate()}/{negociacao.data.getMonth() + 1}/{negociacao.data.getFullYear()}</td>
+                                    <td>{negociacao.quantidade}</td>
+                                    <td>{negociacao.valor}</td>
+                                    <td>{negociacao.volume}</td>
+                                </tr>
+                            )
+                        }
                         )
                     }
-                    )
-                }
-            </tbody>
-            <tfoot>
-          <tr>
-            
-          </tr>
-        </tfoot>
-        </table>
-    )
+                </tbody>
+                <tfoot>
+                    <tr>
+
+                    </tr>
+                </tfoot>
+            </table>
+        )
 }
+
 
 Tabela.propTypes = {
     negociacao: PropTypes.array.isRequired,
 }
 
 export default Tabela;
-
-
-
-
-
-
-
-
-{/* props.negociacoes.map(negociacao => {
-                        return (
-                            <tr key={negociacao.id}>
-                                <td>{negociacao.data.getDate()}/{negociacao.data.getMonth() + 1}/{negociacao.data.getFullYear()}</td>
-                                <td>{negociacao.quantidade}</td>
-                                <td>{negociacao.valor}</td>
-                                <td>{negociacao.volume}</td>
-                            </tr>
-                        // ) */}
